2debba4422b2696fdf0df7a690cc74cc1b3cc154
Yang Zhou <yz96@illinois.edu>
2023-02-06 20:50:15 -0600

    improve 0.70

--- mp1/cp2.py
+++ mp1/cp2.py
@@ -1,5 +1,6 @@
 from user import User
 from utils import distance_km
+import numpy as np
 
 
 def cp2_1_simple_inference(dictUsers):
@@ -28,8 +29,56 @@ def cp2_1_simple_inference(dictUsers):
 
 def cp2_2_improved_inference(dictUsers):
     dictUsersInferred = dict()
-    # TODO
+    for v in dictUsers.values():
+        if v.home_shared == False:
+            frnShareLoc = 0
+            frnShareBound = 5
+            frn_home_loc = []
+            for f in v.friends:
+                frn = dictUsers[f]
+                if frn.home_shared == True:
+                    frnShareLoc += 1
+                    frn_home_loc.append([frn.home_lat, frn.home_lon])
+            if frnShareLoc < frnShareBound:
+                for f in v.friends:
+                    if frnShareLoc >= frnShareBound:
+                            break 
+                    for ff in dictUsers[f].friends:
+                        ffrn = dictUsers[ff]
+                        if ffrn.home_shared == True:
+                            frnShareLoc += 1
+                            frn_home_loc.append([frn.home_lat, frn.home_lon])
+                        if frnShareLoc >= frnShareBound:
+                            break
+            # find the outlier
+            if frn_home_loc != []:
+                frn_home_loc = np.array(frn_home_loc)
+                mean = np.mean(frn_home_loc, axis=0)
+                meanLat = mean[0]
+                meanLon = mean[1]
+                sd = np.std(frn_home_loc, axis=0)
+                sdLat = sd[0]
+                sdLon = sd[1]
+                # float64 accuracy problem +- 10e-8
+                withoutOutlierList = [[x, y] for [x, y] in frn_home_loc if ((x > meanLat - sdLat - 10e-8 and x < meanLat + sdLat + 10e-8) and (y > meanLon - sdLon - 10e-8 and y < meanLon + sdLon + 10e-8))]
+                if len(withoutOutlierList) != 0:
+                    latSum = 0
+                    lonSum = 0
+                    for [lat, lon] in withoutOutlierList:
+                        latSum += lat
+                        lonSum += lon
+                    user = User(v.id, latSum/len(withoutOutlierList), lonSum/len(withoutOutlierList), True)
+                    dictUsersInferred.update(((v.id, user),))
+                else:
+                    user = User(v.id, meanLat, meanLon, True)
+                    dictUsersInferred.update(((v.id, user),))
+            else:
+                user = User(u_id = v.id, u_home_shared = False)
+                dictUsersInferred.update(((v.id, user),))
+        else:
+            dictUsersInferred.update(((v.id, v),))
     return dictUsersInferred
+
 
 
 def cp2_calc_accuracy(truth_dict, inferred_dict):
