25614250cf21aea1778ed7b924d75040c352497e
Yang Zhou <yz96@illinois.edu>
2023-04-18 22:54:40 -0500

    define rules

---
+++ mp5/cp1/rules/rule_capt.py
@@ -0,0 +1,43 @@
+import itertools
+
+def check_capt(pw1,pw2):
+    #pw1,pw2 (string,string): a pair of input password
+    #output (boolean): if pw1 and pw2 can be transformed by this category of rule
+    #e.g. pw1 = abcdE, pw2= abCde, output =True
+    
+    if len(pw1) != len(pw2):
+        return False
+    
+    pw1_lower = pw1.lower()
+    p2_lower = pw2.lower()
+
+    if pw1_lower == p2_lower:
+        return True
+
+    return False 
+
+def check_capt_transformation(pw1, pw2):
+    #pw1,pw2 (string,string): a pair of input password
+    #output (string): transformation between pw1 and pw2
+    #consider if head char is capt transformed, if tail char is capt transformed, and # of chars that has been capt transformed in total
+    #example pw1 = abcde, pw2 = AbcDe, transformation = head\t2 (head char is capt transformed, in total 2 chars are capt transformed)
+    #example pw1 = abcdE, pw2 = AbcDe, transformation = head\ttail\t3 (head char and tail chars are capt transformed, in total 3 chars are capt transformed)
+    #example pw1 = abcde, pw2 = abcDe, transformation = 1 (in total 1 chars are capt transformed)
+
+    # ***********************************************************************
+    # ****************************** TODO ***********************************
+    # ***********************************************************************
+
+    return ''
+
+def apply_capt_transformation(ori_pw, transformation):
+    #ori_pw (string): input password that needs to be transformed
+    #transformation (string): transformation in string
+    #output (list of string): list of passwords that after transformation (all possiblities)
+    #ori_pw = "abcde", transformation = "head\t2", output = [ABcde, AbCde, AbcDe]
+    
+    # ***********************************************************************
+    # ****************************** TODO ***********************************
+    # ***********************************************************************
+
+    return []
---
+++ mp5/cp1/rules/rule_substring.py
@@ -0,0 +1,46 @@
+def check_substring(pw1,pw2):
+    #pw1,pw2 (string,string): a pair of input password
+    #output (boolean): if pw1 and pw2 can be considered as substring of the other 
+    # eg. pw1 = abc, pw2 = abcd, output true
+    # eg. pw1 = abcde, pw2 = abcd, output true
+
+    if pw1 in pw2 or pw2 in pw1:
+        return True
+    return False
+
+def check_substring_transformation(pw1, pw2):
+    #pw1,pw2 (string,string): a pair of input password
+    #output (string): transformation between pw1 and pw2
+    #example: pw1=123hello!!, pw2=hello, output=head\t123\ttail\t!!
+    #example: pw1=hello!!, pw2=hello, output=head\t\ttail\t!!
+
+    # ***********************************************************************
+    # ****************************** TODO ***********************************
+    # ***********************************************************************
+
+    return ''
+
+def guess_target_as_substring(ori_pw):
+    #the first transformation applied in rule_substring
+    #guess the possible passwords as a substring
+    #decide to only consider the substring from head or from tail
+    #e.g. pw1=abc123, output = [a,ab,abc,abc1,abc12,3,23,123,c123,bc123]
+    #in transformation dictionary, the transformation = 'special_trans_as_substring'
+
+    # ***********************************************************************
+    # ****************************** TODO ***********************************
+    # ***********************************************************************
+
+    return []
+
+def apply_substring_transformation(ori_pw, transformation):
+    #ori_pw (string): input password that needs to be transformed
+    #transformation (string): transformation in string
+    #output (list of string): list of passwords that after transformation
+    #add head string to head, add tail string to tail
+    
+    # ***********************************************************************
+    # ****************************** TODO ***********************************
+    # ***********************************************************************
+
+    return []
